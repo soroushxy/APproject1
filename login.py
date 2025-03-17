ee7fd0f2c77e36cdf0da77f2335af3d1a1787c8e
karen-git-evb <karenhamidi10@gmail.com>
2025-03-17 15:38:19 +0330

    create the class for login and signup!

--- login.py
+++ login.py
@@ -12,3 +12,31 @@ def create_database():
     conn.commit()
     conn.close()
 
+
+# create class for login and signup :
+class UserManager:
+    def __init__(self, db_name="users.db"):
+        self.db_name = db_name
+
+    def signup(self, username, password, email):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        try:
+            cursor.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+                           (username, password, email))
+            conn.commit()
+            return True, "Signup successful!"
+        except sqlite3.IntegrityError:
+            return False, "Username or email\n  already exists!"
+        finally:
+            conn.close()
+
+    def login(self, username, password):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user = cursor.fetchone()
+        conn.close()
+        if user:
+            return True, "Login successful!"
+        return False, "Invalid username \n or password!"
